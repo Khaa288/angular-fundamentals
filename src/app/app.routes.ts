cf0b0757df5889e51a45a5696ae9304bfe34312f
Khang Huỳnh <89777765+Khaa288@users.noreply.github.com>
2025-01-18 20:32:04 +0700

    merge main to routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'user', component: UserProfileComponent },
+    { path: 'login', component: LoginComponent }
+];
