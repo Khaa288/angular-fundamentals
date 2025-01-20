267d1e0fd2d56fdd90df0b0599bcdd397c279884
ORIENTSOFTWARE\khang.huynh <khanghuynh.orient@gmail.com>
2025-01-20 17:27:17 +0700

    still adding relative routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,29 @@ import { Routes } from '@angular/router';
 import { PageLoginComponent } from './features/authentication/page-login/page-login.component';
 import { PageUserProfileComponent } from './features/user-profile/page-user-profile/page-user-profile.component';
 import { PageNotFoundComponent } from './features/page-not-found/page-not-found.component';
+import { PageUserProfileSearchComponent } from './features/user-profile/page-user-profile-search/page-user-profile-search.component';
 
 const AppName = "Angular Fundamentals";
 
 export const routes: Routes = [
     { path: '', title: `${AppName} • Login`, component: PageLoginComponent },
     { path: 'login', title: `${AppName} • Login`, component: PageLoginComponent },
-    { path: 'user', title: `${AppName} • User Profile`, component: PageUserProfileComponent },
+    { 
+        path: 'user', 
+        title: `${AppName} • User Profile`, 
+        component: PageUserProfileComponent, 
+        children: [
+            {
+                path: 'detail',
+                title: `${AppName} • User Profile Detail`, 
+                component: PageUserProfileComponent
+            },
+            {
+                path: 'search',
+                title: `${AppName} • User Profile Search`, 
+                component: PageUserProfileSearchComponent
+            }
+        ]
+    },
     { path: '**', title: `${AppName} • NotFound`, component: PageNotFoundComponent }
 ];
--- src/app/features/user-profile/page-user-profile/page-user-profile.component.ts
+++ src/app/features/user-profile/page-user-profile/page-user-profile.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { ApiServiceService } from '../../../core/services/api-service/api-service.service';
+import { UserProfile } from '../../../core/models/user-profile';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'page-page-user-profile',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './page-user-profile.component.html',
   styleUrl: './page-user-profile.component.scss'
 })
 export class PageUserProfileComponent {
+  users : UserProfile[] = [];
   userName = "pro_programmer_123";
   isValidUserId = true;
+
+  constructor(private apiService: ApiServiceService) {
+    this.users = this.apiService.getUsers();
+
+    console.log(this.users);
+  }
 
   disableAll() {
     this.userName = this.isValidUserId ? "disabled-user" : "pro_programmer_123";
