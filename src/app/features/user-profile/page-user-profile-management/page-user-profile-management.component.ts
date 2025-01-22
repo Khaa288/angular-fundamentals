e4d9f1c287a101423a2c9f21234d9b6c82378efe
ORIENTSOFTWARE\khang.huynh <khanghuynh.orient@gmail.com>
2025-01-22 14:07:30 +0700

    done reactive forms

--- src/app/features/user-profile/page-user-profile-management/page-user-profile-management.component.ts
+++ src/app/features/user-profile/page-user-profile-management/page-user-profile-management.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-page-user-profile-management',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './page-user-profile-management.component.html',
   styleUrl: './page-user-profile-management.component.scss'
 })
 export class PageUserProfileManagementComponent {
-  addProfileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl(''),
-    }),
-  });
+  profileForm: FormGroup;
+  profileFormUsingBuilder: FormGroup; 
+
+  constructor(private formBuilder: FormBuilder) {
+    this.profileForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      address: new FormGroup({
+        street: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl(''),
+        zip: new FormControl(''),
+      }),
+    });
+  
+    this.profileFormUsingBuilder = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      address: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: ['']
+      })
+    });
+  }
+
+  onProfileFormSubmit() {
+    console.log(this.profileForm.value);
+  }
+
+  onProfileFormBuilderSubmit() {
+    console.log(this.profileFormUsingBuilder.value);
+  }
 }
